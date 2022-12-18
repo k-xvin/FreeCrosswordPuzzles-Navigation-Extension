1c2260f4220b2de1f078a09b4fee58c3060efe8a
Kevin Xu <kevinshao9@gmail.com>
2022-12-18 12:26:37 -0800

    seems to be fully working

--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
 const clueBoxes = getClueBoxes();
-const acrossClueNumbers = getClues("frAcross"); 
+const acrossClueNumbers = getClues("frAcross");
 const downClueNumbers = getClues("frDown");
 
-function getClues(clueIFrameId){
-    const divClues = document.getElementById("frAcross").contentWindow.document.getElementsByTagName("div");
+function getClues(clueIFrameId) {
+    const divClues = document.getElementById(clueIFrameId).contentWindow.document.getElementsByTagName("div");
     let clues = Array.from(divClues).map(
         (d) => {
             // slice the divClue text off of divClue#
@@ -15,7 +15,7 @@ function getClues(clueIFrameId){
     return clues;
 }
 
-function getClueBoxes(){
+function getClueBoxes() {
     const boxNumbers = document.getElementById("tblCrossword").querySelectorAll("td div:first-child");
     // this also maps one of the blank squares, but that's okay
     let mappedBoxes = new Map(Array.from(boxNumbers).map((b) => {
@@ -26,62 +26,101 @@ function getClueBoxes(){
     return mappedBoxes;
 }
 
-// function getClueSquare(clueNum){
-//     console.log(document.getElementById("tblCrossword").querySelectorAll("td div:first-child"));
-// }
-
 let currentClueNumber = 1;
-let acrossMode = false;
 
-function nextAcross(){
-    for(let i=0;i<acrossClueNumbers.length;i++){
-        let clueNumber = acrossClueNumbers[i];
-        if(clueNumber>currentClueNumber){
-            // console.log(clueBoxes.get(num).parentElement);
+// odd# == across mode / even# == down mode
+// let numClicks = 0;
+const Mode = {
+    ACROSS: true,
+    DOWN: false
+}
+let currentMode = Mode.ACROSS;
+document.getElementById("tblCrossword").addEventListener("click", (event) => {
+    // The down/across mode flips when you click a box
+    currentMode = !currentMode;
+    
+    // depending on where someone clicked, update the position
+    currentClueNumber = getCurrentClue();
+});
 
-            // double click to retain across mode
-            clueBoxes.get(clueNumber).parentElement.dispatchEvent(new Event('click'));
-            clueBoxes.get(clueNumber).parentElement.dispatchEvent(new Event('click'));
-
-            // cycle cluenumber back to start when we reach end
-            currentClueNumber = clueNumber;
-            if(i == acrossClueNumbers.length-1) currentClueNumber = 0;
-            return;
+function getCurrentClue() {
+    if (currentMode == Mode.ACROSS) {
+        // find the first highlighted across clue and return the number
+        const divClues = document.getElementById("frAcross").contentWindow.document.getElementsByTagName("div");
+        for (let i = 0; i < divClues.length; i++) {
+            let d = divClues[i];
+            if (d.style.backgroundColor) {
+                return Number(d.id.slice("divClue".length));
+            }
+        }
+    }
+    else {
+        // find the first highlighted down clue and return the number
+        const divClues = document.getElementById("frDown").contentWindow.document.getElementsByTagName("div");
+        for (let i = 0; i < divClues.length; i++) {
+            let d = divClues[i];
+            if (d.style.backgroundColor) {
+                return Number(d.id.slice("divClue".length));
+            }
         }
     }
 }
 
-function prevAcross(){
 
+function traverseNext(clueArray) {
+    // find and go to the next element
+    for (let i = 0; i < clueArray.length; i++) {
+        let clueNumber = clueArray[i];
+        if (clueNumber > currentClueNumber) {
+            currentClueNumber = clueNumber;
+            doubleClick(clueBoxes.get(clueNumber).parentElement);
+            return;
+        }
+    }
+    // no element found, so we need to go to the start
+    currentClueNumber = 1;
+    doubleClick(clueBoxes.get(currentClueNumber).parentElement);
 }
 
-function nextDown(){
-
+function traversePrev(clueArray) {
+    // find and go to the next preceding element
+    for (let i = clueArray.length - 1; i >= 0; i--) {
+        let clueNumber = clueArray[i];
+        if (clueNumber < currentClueNumber) {
+            currentClueNumber = clueNumber;
+            doubleClick(clueBoxes.get(clueNumber).parentElement);
+            return;
+        }
+    }
+    // no element found, so we need to go to the end
+    currentClueNumber = clueArray[clueArray.length - 1];
+    doubleClick(clueBoxes.get(currentClueNumber).parentElement);
 }
 
-function prevDown(){
-
+function doubleClick(element) {
+    // double click to retain across mode
+    element.dispatchEvent(new Event('click'));
+    element.dispatchEvent(new Event('click'));
 }
-
-// function clickSquare(row, col){
-//     document.getElementById(`T${col}x${row}`).dispatchEvent(new Event('click'));
-// }
-
 
 document.addEventListener("keydown", (event) => {
-    if(event.key == 'Tab'){
-        if(event.shiftKey){
-            console.log("shifttabbed")
+    if (event.key == 'Tab') {
+        if (event.shiftKey) { // shift+tab
+            if (currentMode == Mode.ACROSS) {
+                traversePrev(acrossClueNumbers);
+            }
+            else {
+                traversePrev(downClueNumbers);
+            }
         }
-        else {
-            console.log("tabbed")
-            // getClueSquare(3);
-            nextAcross();
+        else { // tab
+            if (currentMode == Mode.ACROSS) {
+                traverseNext(acrossClueNumbers);
+            }
+            else {
+                traverseNext(downClueNumbers);
+            }
         }
-        
-        // document.getElementById("T1x1").click(); // .click() doesn't work
-        // document.getElementById("T1x14").dispatchEvent(new Event('click'));
-
     }
 });
 
